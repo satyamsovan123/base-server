1a8d95f0db1ebd5df084b6b123421113d76e62ed
Satyam Sovan Mishra <msatyamsovan@gmail.com>
2024-02-04 17:16:01 +0530

    Test scheduler

--- utils/scheduler.js
+++ utils/scheduler.js
@@ -1,21 +1,19 @@
 const { logger } = require("./logger");
-var cron = require("node-cron");
+const cron = require("node-cron");
 const { sendCampaignEmail } = require("./sendCampaignEmail");
 const { Data } = require("../app/models");
 const { appConfig } = require("../configs/appConfig");
 
 async function scheduler() {
   logger(`UTILS / SCHEDULER - Inside scheduler`);
-
-  // 0 50 15 * * *
-  cron.schedule("*/15 * * * *", async () => {
+  // 0 30 08 * * * - At 08:30:00 AM every day
+  // */10 * * * * - Every 10 minutes
+  cron.schedule("*/10 * * * *", async () => {
     const allData = await Data.find({}).select("title article email");
     const randomData = allData[Math.floor(Math.random() * allData.length)];
 
-    logger(`UTILS / SCHEDULER - Random data - ${JSON.stringify(randomData)}`);
-
     logger(
-      `UTILS / SCHEDULER - Inside cron job that runs every day at 14:57:00 PM`
+      `UTILS / SCHEDULER - Inside cron job that runs every day at 08:30:00 AM`
     );
     await sendCampaignEmail(
       appConfig.testEmail,
