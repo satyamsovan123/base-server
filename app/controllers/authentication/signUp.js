78393a607ed2ad9fa9b5095d97d096410dbdd82e
Satyam Sovan Mishra <msatyamsovan@gmail.com>
2024-02-08 10:54:12 +0530

    Add req logger

--- app/controllers/authentication/signUp.js
+++ app/controllers/authentication/signUp.js
@@ -11,7 +11,7 @@ const { excryptPlainText, generateJWT, sendOTP } = require("../../../utils");
 const signUp = async (req, res) => {
   try {
     logger(`CONTROLLERS / SIGNUP - Inside sign up`);
-    logger(`CONTROLLERS / SIGNUP - Req - ${JSON.stringify(req)}`);
+    logger(`CONTROLLERS / SIGNUP - Req - ${req}`);
 
     const userData = req.body;
     logger(`CONTROLLERS / SIGNUP - User - ${userData.email}`);
