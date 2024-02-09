13ec73be45756f981a776c7f95970c3ace5ae7b0
Satyam Sovan Mishra <msatyamsovan@gmail.com>
2024-02-09 10:04:28 +0530

    Add cookie logger

--- app/controllers/authentication/signIn.js
+++ app/controllers/authentication/signIn.js
@@ -49,6 +49,7 @@ const signIn = async (req, res) => {
         secure: true,
         httpOnly: true,
         sameSite: "none",
+        path: "/",
         maxAge: 86400 * 1000,
       })
       .setHeader(serverConstant.AUTHORIZATION_HEADER_KEY, `Bearer ${token}`)
