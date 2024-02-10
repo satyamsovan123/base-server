7a3b1b8be3029e441f30e55fcce8960f348d61b2
Satyam Sovan Mishra <msatyamsovan@gmail.com>
2024-02-10 12:30:36 +0530

    Add headers

--- app/controllers/authentication/signIn.js
+++ app/controllers/authentication/signIn.js
@@ -49,7 +49,7 @@ const signIn = async (req, res) => {
       .cookie("Bearer", `${token}`, {
         httpOnly: true,
         secure: true,
-        sameSite: "strict",
+        sameSite: "lax",
         path: "/",
         maxAge: 86400 * 1000,
       })
