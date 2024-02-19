ca3d87eb6f7b68f1b4ee87f8531557f73481b4e5
Satyam Sovan Mishra <msatyamsovan@gmail.com>
2024-02-19 15:46:23 +0530

    Add memory storage in multer

--- configs/fileStorageConfig.js
+++ configs/fileStorageConfig.js
@@ -1,6 +1,6 @@
 const multer = require("multer");
 const { uuid } = require("uuidv4");
-const storage = multer.diskStorage({
+const storage = multer.memoryStorage({
   destination: function (req, file, cb) {
     cb(null, "uploads/");
   },
